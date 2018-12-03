90c21798947e3fd02b8d9ee8ccec5080eca73c89
psmith150 <patricksmith150@gmail.com>
2018-12-02 22:12:33 -0600

    Update docker files to connect to SQL database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-tutorial',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'DEFAULT-CHARACTER-SET': 'utf8',
+        'HOST': 'db',
+        'PORT': 3306
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
